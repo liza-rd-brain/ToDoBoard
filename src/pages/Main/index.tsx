82c52464ce8e1a6a43698663d332324fdce794c6
Elizabeth <33760912+MinakovaLisa@users.noreply.github.com>
2022-11-28 21:12:50 +0300

    raw main page

---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+import { db, firebaseConfig } from "../../firebase";
+import style from "./index.module.scss";
+
+export const Main = () => {
+  //Подобие header +логотип на индекс
+  //кнопка создать проект
+  // выбрать проект
+
+  return (
+    <div className={style.container}>
+      <header className={style.header}>
+        <div className={style.logo}></div>
+      </header>
+      <div className={style.projectList}>
+        <button className={style.button}>+</button>
+
+        {/* Далее иконки проектов */}
+      </div>
+    </div>
+  );
+};
